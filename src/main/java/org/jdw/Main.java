34acd3a1ca9e5854af20c3a3e08b26ad3105e25c
Justin Wilson <jdwilson11@gmail.com>
2014-03-02 23:20:37 -0600

    Added the Google Mirror API quickstart MirrorClient class. Standardized formatting and cleaned up Gradle dependencies. Organized packages.

--- src/main/java/org/jdw/Main.java
+++ src/main/java/org/jdw/Main.java
@@ -13,15 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+	public static void main(String[] args) throws Exception {
+		ApplicationContext ctx = SpringApplication.run(Main.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
 }
